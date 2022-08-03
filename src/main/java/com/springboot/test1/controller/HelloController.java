3cadb21df46f92fcef033455f11a4cba17e24f7a
rovigos-byounggoun <kgun610@hanmail.net>
2022-08-04 01:22:06 +0900

    swagger 테스트 추가

--- src/main/java/com/springboot/test1/controller/HelloController.java
+++ src/main/java/com/springboot/test1/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.springboot.test1.controller;
 
 import com.springboot.test1.data.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -10,8 +12,11 @@ import java.util.Map;
 public class HelloController {
 
     //http://localhost:8080/api/v1/get=api/hello/{variable/?a=a&b=b
+    @ApiOperation(value = "GET 예제", notes = "@Request Get")
     @GetMapping(value = "/hello/{variable}")
-    public String hello(@PathVariable("variable") String var, @RequestParam Map<String, String> param){
+    public String hello(
+            @PathVariable("variable") String var,
+            @ApiParam(value = "param", required = true) @RequestParam Map<String, String> param){
         StringBuilder sb = new StringBuilder();
         param.entrySet().forEach(map -> {
             sb.append(map.getKey() + " : " + map.getValue() + "\n");
